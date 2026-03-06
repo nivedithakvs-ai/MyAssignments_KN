43f083dad5e202fe33dc02225a9f5db24e987479
nivedithakvs-ai <nivedithakvs@gmail.com>
2026-03-07 00:00:52 +0530

    Add files via upload

---
+++ datatypeday01.js
@@ -0,0 +1,14 @@
+let firstName = `Niveditha`
+var companyName = "Accenture"
+let mobileNumber = 9094722733
+var isAutomation = false
+var hasPlaywright
+console.log("My First Name is "+firstName)
+console.log("I work in " +companyName)
+console.log("My contact number is "+mobileNumber)
+console.log("Automation : "+isAutomation)
+console.log(typeof firstName)
+console.log(typeof companyName)
+console.log(typeof mobileNumber)
+console.log(typeof isAutomation)
+console.log(typeof hasPlaywright)
